accac1d5cf05a4a51faa671a9219e08683ca2340
zonesan <san@zoobook.org>
2016-11-03 17:34:35 +0800

    init

---
+++ index.js
@@ -0,0 +1,20 @@
+var express = require('express')
+var stringifyObject = require('stringify-object')
+var app = express()
+
+app.get('*', function (req, res) {
+    var headersJson = stringifyObject(req.headers,{
+        indent:'  ',
+        singleQuotes: false
+    });
+    console.log(req.url)
+    console.log(headersJson);
+    
+    headersJson='<pre><code>'+headersJson+'</code></pre>';
+
+    res.send(headersJson);
+})
+
+app.listen(8080, function () {
+    console.log('Example app listening on port 8000!');
+})
